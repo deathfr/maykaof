6722d6c09fbd648330ae6dc625ff0569c5bf02d0
deathfr <death.fr05t.7777@gmail.com>
2025-12-12 23:47:40 +0100

    Add files via upload

--- netlify/functions/getPosts.js
+++ netlify/functions/getPosts.js
@@ -1,14 +1,36 @@
 import { getStore } from "@netlify/blobs";
 
 export async function handler() {
-  const store = getStore("posts");
-  const data = await store.get("posts");
+  try {
+    const store = getStore("posts");
+    const data = await store.get("posts");
 
-  return {
-    statusCode: 200,
-    body: data || "[]",
-    headers: {
-      "Content-Type": "application/json"
+    let posts = [];
+
+    if (data) {
+      try {
+        posts = JSON.parse(data);
+        if (!Array.isArray(posts)) posts = [];
+      } catch {
+        posts = [];
+      }
     }
-  };
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(posts)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 200, // UWAGA: celowo 200
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: "[]"
+    };
+  }
 }
